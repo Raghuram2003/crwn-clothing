2805eb1bad301629ef28a1c2bdeb5b475c94089b
Raghuram2003 <destrobroxd@gmail.com>
2023-09-05 13:48:59 +0530

    added sign-up page using firebasse

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,16 +1,20 @@
-import {signInWithGooglePopup , createUserDocumentFromAuth} from "../../utils/firbase/firbase.utils"
-
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firbase/firbase.utils";
+import SignUpForm from "../sign-up/sign-up.component";
 export const SignIn = () => {
-    const logGoogleUser = async()=>{
-        const {user} = await signInWithGooglePopup();
-        const userDocRef = await  createUserDocumentFromAuth(user)
-    }
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+    console.log(userDocRef);
+  };
+
   return (
     <div>
       <h1>sign in</h1>
-      <button onClick={logGoogleUser}>
-        sign in with google popup
-      </button>
+      <button onClick={logGoogleUser}>sign in with google popup</button>
+      <SignUpForm/>
     </div>
   );
 };
--- src/utils/firbase/firbase.utils.jsx
+++ src/utils/firbase/firbase.utils.jsx
@@ -4,6 +4,7 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from "firebase/auth";
 
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
@@ -27,10 +28,17 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation
+) => {
+  if (!userAuth) return;
+
   const userDocRef = doc(db, "users", userAuth.uid);
   console.log(userDocRef);
 
@@ -47,10 +55,16 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        ...additionalInformation,
       });
     } catch (error) {
       console.log("There was an error creating an user ", error.message);
     }
   }
   return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
 };
